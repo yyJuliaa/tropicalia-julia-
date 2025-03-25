dbcca191f46b465e6831d3356f9c9d2700b15db5
yyJuliaa <160231902+yyJuliaa@users.noreply.github.com>
2025-03-25 12:23:20 +0000

    iniciamos A AULA 55

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiuFonteBotao= document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -7,4 +9,11 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+
+    diminuiuFonteBotao.addEventListener('click', function(){
+           tamanhoAtualFonte -= 0.1;
+           document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+   
+       });
 });
+
